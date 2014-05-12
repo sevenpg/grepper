60a1e8894113a09377270cd185b84422b809d153
Kohei Tsuruta <seventhepg@gmail.com>
2014-05-12 15:47:23 +0900

    Use async process

--- grepper.el
+++ grepper.el
@@ -1,4 +1,5 @@
-(defvar grepper-buffer "*grepper result*")
+(defvar grepper-buffer-name "*grepper result*")
+(defvar grepper-cat-path "/bin/cat")
 
 (defun grepper--read-pattern ()
   (let* ((default (thing-at-point 'symbol))
@@ -7,10 +8,29 @@
         default
       pattern)))
 
+(defun grepper--filter (proc output)
+  (with-current-buffer (process-buffer proc)
+    (insert output)))
+
+(defun grepper--sentinel (proc status)
+  (if (equal status "finished\n")
+      (pop-to-buffer (process-buffer proc))))
+
+(defmacro grepper--with-cat-pager (&rest body)
+  `(let ((pager (make-symbol "pager")))
+     (setq pager (getenv "PAGER"))
+     (setenv "PAGER" grepper-cat-path)
+     (unwind-protect
+        ,@body
+       (setenv "PAGER" pager))))
+
 (defun grepper (pattern)
   (interactive (list (grepper--read-pattern)))
-  (let ((buffer (get-buffer-create grepper-buffer)))
+  (let ((buffer (get-buffer-create grepper-buffer-name))
+        proc)
     (with-current-buffer buffer
-      (erase-buffer)
-      (call-process "git" nil buffer nil "grep" "-n" pattern))
-    (pop-to-buffer buffer)))
+      (erase-buffer))
+    (grepper--with-cat-pager
+      (setq proc (start-process "grepper" buffer "git" "grep" "-n" (shell-quote-argument pattern))))
+    (set-process-sentinel proc 'grepper--sentinel)
+    (set-process-filter proc 'grepper--filter)))
