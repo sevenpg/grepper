f5a3d42809e2101bd63dd723b9a68d852d44afe9
Kohei Tsuruta <seventhepg@gmail.com>
2014-05-12 15:07:23 +0900

    Set thing-at-point as default

--- grepper.el
+++ grepper.el
@@ -1,10 +1,16 @@
 (defvar grepper-buffer "*grepper result*")
 
+(defun grepper--read-pattern ()
+  (let* ((default (thing-at-point 'symbol))
+         (pattern (read-string (format "Pattern (default %s): " default))))
+    (if (eq (length pattern) 0)
+        default
+      pattern)))
+
 (defun grepper (pattern)
-  (interactive "sPattern: ")
+  (interactive (list (grepper--read-pattern)))
   (let ((buffer (get-buffer-create grepper-buffer)))
-    (save-excursion
-      (set-buffer buffer)
-      (erase-buffer))
-    (call-process "git" nil buffer nil "grep" "-n" pattern)
+    (with-current-buffer buffer
+      (erase-buffer)
+      (call-process "git" nil buffer nil "grep" "-n" pattern))
     (pop-to-buffer buffer)))
